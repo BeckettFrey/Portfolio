9956a8c92e6445a6055b10ef26f8ab1deea0574c
Beckett Frey <beckett.frey@gmail.com>
2022-12-20 19:12:33 -0600

    Done

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-var main3 = document.querySelectorAll("button.main-3");
+// var main3 = document.querySelectorAll("button.main-3");
 
-for (let i = 0; i < main3.length; i++) {
-  main3[i].addEventListener("click", () => {
-    main3[i].innerHTML = "clicked";
-  });
-}
-var slideIndex = 1;
-showSlides(slideIndex);
+// for (let i = 0; i < main3.length; i++) {
+//   main3[i].addEventListener("click", () => {
+//     main3[i].innerHTML = "clicked";
+//   });
+// }
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+// slider[0].childNodes[1].addEventListener("onfd", () => {
+// });
