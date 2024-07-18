ed01443af9e3da4a82d1b22b008d36a476dd18c6
Beckett Frey <beckett.frey@gmail.com>
2024-07-18 14:56:50 -0500

    decluttered and updated

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(3000);
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+
+app.get("/", (res) => {
+  const root = path.join(__dirname, "public", "index.html");
+  res.sendFile(root);
 });
