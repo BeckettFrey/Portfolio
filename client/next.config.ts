d9e0acc517b91cd7e60a5eb36cfb88ca03bcf057
Beckett Frey <beckett.frey@gmail.com>
2025-07-31 02:45:45 -0500

    net: add route for backdoor

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['covers.openlibrary.org'],
   },
+  async redirects() {
+    return [
+      {
+        // Redirect to the latest resume PDF
+        source: '/resume',
+        destination: 'https://github.com/beckettfrey/latex-resume/releases/latest/download/resume.pdf',
+        permanent: false,
+      },
+    ];
+  },
 }
 
 export default nextConfig;
